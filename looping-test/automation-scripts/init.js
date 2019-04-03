e60cca6ae7c1c604d199f19ef580c902393af78a
hannahintech <hannahtuckermclellan@gmail.com>
2019-04-03 11:46:21 +0100

    js is being added to json and created as a json file inside the component folder

--- looping-test/automation-scripts/init.js
+++ looping-test/automation-scripts/init.js
@@ -1,43 +1,10 @@
 const fs = require('fs');
 const path = require('path');
-const process = require("process");
 
 const baseFolderLocation = "/Users/hannah/Documents/sandbox/storybook-to-json/looping-test/src";
-const outputFileName = './../json-outputs/looper-output.json';
-const fileMatch = /\.stories.tsx$/;
-let results = [];
 let discards = [];
+// TODO we don't need discards to be stored! get rid!
 let stringifiedFiles = [];
-
-
-let jsonObjectForFrontify = {
-    "name": "Paragraph",
-    "description": "Basic Paragraph",
-    "type": "atom",
-    "stability": "stable",
-    "variations": {
-        "lead": {
-            "name": "Lead paragraph",
-            "assets": {
-                "html": [
-                    "test/fixtures/patterns/atoms/paragraph/paragraph_lead.html"
-                ],
-                "css": [
-                    "test/fixtures/patterns/atoms/paragraph/css/paragraph_lead.css"
-                ]
-            }
-        }
-    },
-    "assets": {
-        "html": [
-            "test/fixtures/patterns/atoms/paragraph/paragraph.html"
-        ],
-        "css": [
-            "test/fixtures/patterns/atoms/paragraph/css/paragraph.css"
-        ],
-        "js": ""
-    }
-};
 
 /**
  * Walks though all the folders and sub folders in a given path and looks at what that file type is.
@@ -53,70 +20,73 @@ function walkThoughDirectories(dir) {
             discards = discards.concat(walkThoughDirectories(file));
         } else {
             if (file.includes('stories.tsx')) {
-                results.push(file);
+                fs.readFile(file, 'utf8', (err, fileContents) => {
+                    // 2. Pull out JS contnet
+                    let stringifiedFile = JSON.stringify(fileContents);
+                    // console.log("stringy file      " + JSON.parse(stringifiedFile))
+
+                    // 3. find specific js elements needed
+                    let strArr = stringifiedFile.split(" ");
+                    // console.log("stringifiedFile        " + stringifiedFile)
+                    let jsStart = strArr.indexOf('let');
+                    let jsEnd = strArr.indexOf('return');
+                    let tagNameStart = strArr.indexOf('createElement') + 2;
+                    let tagNameEnd = strArr.indexOf(')', tagNameStart) - 1;
+        
+                    // 4. make string of just the javascript portion of the story
+                    let onlyJsArr = [];
+                    onlyJsArr = strArr.slice(jsStart, jsEnd);
+                    let onlyJs = JSON.stringify(onlyJsArr.join().replace(/,/g, ' '));
+                    //  TODO -> error handling ie. if there is no js (empty string?)
+        
+                    // 5. add extra peice of js - document.body.appendChild(// specific element \\);
+                    let componentNameLocation = strArr.indexOf('let') + 1;
+                    let componentName = strArr[componentNameLocation];
+                    let finalJs = onlyJs.concat(`document.body.appendChild('yoo-${componentName}')`);
+                    console.log("final js!!!                 " + finalJs)
+                    finalJsString = finalJs;
+                    if (finalJs !== null) {
+                        writeJsonOutput(file);
+                    }
+                });
             };
         }
     });
-    return results;
+
 }
 
-/**
- * This function extracts the contents of each .stories.tsx file and finds the JS element of the story
- * and extracts it and turns it into a single string and adds the JS append command to the end of the constructed string
- */
-function readFileContents() {
-    results.forEach(fileLocation => {
-        fs.readFile(fileLocation, 'utf8', (err, fileContents) => {
-            // 2. Pull out JS contnet
-            let stringifiedFile = JSON.stringify(fileContents);
 
-            // 3. find specific js elements needed
-            let strArr = stringifiedFile.split(" ");
-            let jsStart = strArr.indexOf('let');
-            let jsEnd = strArr.indexOf('return');
-            let tagNameStart = strArr.indexOf('createElement') + 2;
-            let tagNameEnd = strArr.indexOf(')', tagNameStart) - 1;
-
-            // 4. make string of just the javascript portion of the story
-            let onlyJsArr = [];
-            onlyJsArr = strArr.slice(jsStart, jsEnd);
-            let onlyJs = JSON.stringify(onlyJsArr.join().replace(/,/g, ' '));
-            // console.log(onlyJs);
-
-            // 5. add extra peice of js - document.body.appendChild(// specific element \\);
-
-            let componentNameLocation = strArr.indexOf('let') + 1;
-            let componentName = strArr[componentNameLocation];
-            let finalJs = onlyJs.concat(`document.body.appendChild('yoo-${componentName}')`);
-            finalJsString = finalJs;
-            if (finalJs !== null) {
-                writeJsonOutput();
+function writeJsonOutput(file) {
+    let jsonObjectForFrontify = {
+        "name": "Paragraph",
+        "description": "Basic Paragraph",
+        "type": "atom",
+        "stability": "stable",
+        "variations": {
+            "lead": {
+                "name": "Lead paragraph",
+                "assets": {
+                    "html": [
+                        "test/fixtures/patterns/atoms/paragraph/paragraph_lead.html"
+                    ],
+                    "css": [
+                        "test/fixtures/patterns/atoms/paragraph/css/paragraph_lead.css"
+                    ]
+                }
             }
-        });
-    });
+        },
+        "assets": {
+            "html": [
+                "test/fixtures/patterns/atoms/paragraph/paragraph.html"
+            ],
+            "css": [
+                "test/fixtures/patterns/atoms/paragraph/css/paragraph.css"
+            ],
+            "js": ""
+        }
+    };
+    jsonObjectForFrontify.assets.js = finalJsString;
+    fs.writeFileSync(`${file}.json`, JSON.stringify(jsonObjectForFrontify));
 }
-
-/**
- * This function is called once the JS function has been extracted from the whole .stories.tsx file
- * It takes the base JSON format needed for Frontify, and inserts the constructed JS string into the JSON
- * It then writes out and saves the JSON in the file system.
- */
-// function writeJsonOutput() {
-//     let 
-//     jsonObjectForFrontify.assets.js = finalJsString;
-//     // console.log(jsonObjectForFrontify);
-//     fs.writeFileSync('frontify-data.json', JSON.stringify(jsonObjectForFrontify));
-// }
-
-
-/**
- * This function is called when the script is run and starts the script.
- */
-// walkThoughDirectories(baseFolderLocation, fileMatch, filename => {
-//     if (filename) {
-//         results.push(filename);
-//     }
-//     readFileContents();
-// });
 
 walkThoughDirectories(baseFolderLocation)
